684e6c2c97fdb6918151dc40316066457a3e9ad7
Wildan Auliya Hafidz <110149128+Crowre@users.noreply.github.com>
2022-11-29 14:22:20 +0700

    Cookie
    mempelajari cookie dan menambahkannya ke dalam file login dan logout

--- belajarPHPpemula/login.php
+++ belajarPHPpemula/login.php
@@ -1,11 +1,28 @@
 <?php
 session_start();
+require 'functions.php';
+
+//cek cookie
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+    $id = $_COOKIE['id'];
+    $key = $_COOKIE['key'];
+
+    //ambil username berdasarkan id
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE
+        id = $id");
+    $row = mysqli_fetch_assoc($result);
+
+    //cek cookie dan username
+    if ($key === hash('sha256', $row['username'])) {
+        $_SESSION['login'] = true;
+    }
+}
 
 if (isset($_SESSION["login"])) {
     header("Location: index.php");
     exit;
 }
-require 'functions.php';
+
 if (isset($_POST["login"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
@@ -21,6 +38,18 @@ if (isset($_POST["login"])) {
         if (password_verify($password, $row["password"])) {
             //set session
             $_SESSION["login"] = true;
+
+            //cek remember me
+            if (isset($_POST["remember"])) {
+
+                //buat cookie
+                setcookie('id', $row['id'], time() + 60);
+                setcookie(
+                    'key',
+                    hash('sha256', $row['username']),
+                    time() + 60
+                );
+            }
             header("Location: index.php");
             exit;
         }
@@ -55,6 +84,10 @@ if (isset($_POST["login"])) {
             <li>
                 <label for="password">password :</label>
                 <input type="text" name="password" id="password">
+            </li>
+            <li>
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember Me</label>
             </li>
             <li>
                 <button type="submit" name="login">Login</button>
--- belajarPHPpemula/logout.php
+++ belajarPHPpemula/logout.php
@@ -4,5 +4,10 @@ session_start();
 session_unset();
 session_destroy();
 
+
+//menghapus cookie
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time(-3600));
+
 header("Location: login.php");
 exit;
